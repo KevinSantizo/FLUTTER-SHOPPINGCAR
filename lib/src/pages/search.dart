566002a7111b258e22dc4fbbcdf3b029b809283c
KevinSantizo <kevinsantizo660@gmail.com>
2020-06-18 23:32:18 -0600

    Whatsapp and call launcher

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_launch/flutter_launch.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,11 +8,52 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+
+  initState() {
+    super.initState();
+  }
+
+  void whatsAppOpen() async {
+    await FlutterLaunch.launchWathsApp(
+      phone: '50247997005', 
+      message: 'Hola teto'
+    );
+  }
+
+  void launchCaller() async {
+    String url = "tel://+50247997005";
+    if (await canLaunch.call(url)) {
+      await launch(url);
+    } else {
+      throw 'No se pudo realizar la llamada';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Buscar Carros'),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.green,
+              onPressed: (){
+                whatsAppOpen();
+              },
+              child: Text('Whatsapp'),
+            ),
+             RaisedButton(
+              color: Colors.green,
+              onPressed: (){
+                launchCaller();
+              },
+              child: Text('Llamar'),
+            ),
+          ],
+        ),
       ),
     );
   }
